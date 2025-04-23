5d00468a02c4a8be7245369f36216ffcc4e938a5
Markmcl25 <146131367+Markmcl25@users.noreply.github.com>
2025-04-23 20:35:31 +0100

    Initial commit. added welcome message via print and added question of where you grew up. definded city as a variable also

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
 print("Welcome to the Band Name Generator")
 city = input("Which city did you grow up in?")
-print(city)
+pet = input("What is the name of a pet")
+print("Your band name could be: " + city + " " + pet)
